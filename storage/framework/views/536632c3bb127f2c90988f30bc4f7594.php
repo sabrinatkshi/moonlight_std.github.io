0fdde7631795c33d68d91fe6d4744859378efd35
sabrinatkshi <140882007+sabrinatkshi@users.noreply.github.com>
2023-07-29 16:58:00 +0700

    Add files via upload

---
+++ storage/framework/views/536632c3bb127f2c90988f30bc4f7594.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Moonlight Studios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aboreto&family=Alex+Brush&family=Allura&family=Calligraffitti&family=Cinzel+Decorative&family=Cookie&family=Dancing+Script:wght@700&family=Elsie&family=Encode+Sans&family=Exo+2:wght@300&family=Fleur+De+Leah&family=Great+Vibes&family=Inter:wght@200;400&family=Italianno&family=Lavishly+Yours&family=Libre+Barcode+128&family=Libre+Baskerville&family=Lovers+Quarrel&family=Marck+Script&family=Merriweather+Sans:wght@300&family=Noto+Sans+Bassa+Vah&family=Nunito+Sans:wght@300;400&family=Oooh+Baby&family=PT+Serif&family=Passions+Conflict&family=Pinyon+Script&family=Playfair+Display:ital,wght@0,400;1,500&family=Prompt&family=Quintessential&family=Signika&family=Signika+Negative:wght@300&family=Urbanist:wght@300&family=Whisper&family=WindSong&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.3.0/css/all.css">
+    <script src="https://site-assets.fontawesome.com/releases/v6.3.0/js/all.js" data-auto-add-css="false" data-auto-replace-svg="false"></script>
+    <link rel="stylesheet" href="<?php echo e(asset('user/vendor/tom-select/dist/css/tom-select.bootstrap5.css ')); ?>">
+
+    <style>
+        body {
+            margin: auto;
+            max-width: 100vw;
+            width: 640px;
+            background: #ebecef;
+            user-select: none;
+        }
+    </style>
+</head>
+
+<body>
+
+    
+    <div class="card" style="margin-top: 180px;border-radius: 25px">
+        <div class="card-body">
+            <b>
+                <p style="font-family: 'Aboreto', cursive;text-align: center;margin-top: 10px;font-size: 22px">Welcome To</p>
+                <div class="d-flex justify-content-center">
+                    <img src="img/font2.png" style="width: 82%;margin-top: -12px" alt="">
+                </div>
+                <div class="d-flex justify-content-center">
+                    <img src="img/studios.png" style="margin-top: -2px;
+                width: 18%;" alt="">
+                </div>
+                <p style="font-family: 'Inter', sans-serif;text-align: center;margin-top: 38px;">Streetwear Graphic Designer</p>
+
+                <div class="d-flex justify-content-center">
+                    <a href="https://instagram.com/moonlight.std_?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D" class="btn" class="btn btn-dark" style=";border-radius: 20px;width: 88%;background-color: black;">
+                        <div class="d-flex justify-content-end">
+                            <h6 class="" style="font-family: 'Prompt', sans-serif;font-size: 14px;color: #ffffff;margin-right: 210px;margin-top: 6px">INSTAGRAM </h6>
+                        </div>
+                        <h6 class="" style="margin-left: 260px;
+                        margin-top: -27px;"><i class="fa-brands fa-instagram fa-lg" style="color: #fcfcfc;"></i> </h6>
+                    </a>
+                </div>
+                <div class="d-flex justify-content-center mt-4">
+                    <a href="mailto:yasinpdlhh@gmail.com?" class="btn" class="btn btn-dark" style=";border-radius: 20px;width: 88%;background-color: black;">
+                        <div class="d-flex justify-content-end">
+                            <h6 class="" style="font-family: 'Prompt', sans-serif;font-size: 14px;color: #ffffff;margin-right: 246px;margin-top: 6px">EMAIL </h6>
+                        </div>
+                        <h6 class="" style="margin-left: 260px;
+                        margin-top: -27px;"><i class="fa-regular fa-envelope fa-lg" style="color: #ffffff;"></i> </h6>
+                    </a>
+                </div>
+                <div class="d-flex justify-content-center mt-4 mb-5">
+                    <a href="#" class="btn" class="btn btn-dark" style=";border-radius: 20px;width: 88%;background-color: black;">
+                        <div class="d-flex justify-content-end">
+                            <h6 class="" style="font-family: 'Prompt', sans-serif;font-size: 14px;color: #ffffff;margin-right: 236px;margin-top: 6px">PAYPAL </h6>
+                        </div>
+                        <h6 class="" style="margin-left: 260px;
+                        margin-top: -27px;"><i class="fa-brands fa-paypal fa-lg" style="color: #ffffff;"></i> </h6>
+                    </a>
+                </div>
+
+        </div>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+</html>
+<?php /**PATH C:\moonlight_studios\moonlight_studios\resources\views/index.blade.php ENDPATH**/ ?>
